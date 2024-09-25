1141319bebc0d3fe4c176567b801840e57a57a20
peterls <peterleite.dev@gmail.com>
2024-09-25 02:03:36 -0300

    Add logic to save the task list in the Local Storge

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Header } from "./components/Header/Header";
 import "./global.css";
 import { Task } from "./types/task";
 import { TaskList } from "./components/TaskList/TaskList";
 import { Footer } from "./components/Footer/Footer";
+
 function App() {
   const [tasks, setTasks] = useState<Task[]>([]);
+
+  // Load tasks from localStorage when the component mounts
+  useEffect(() => {
+    const storedTasks = localStorage.getItem("tasks");
+    if (storedTasks) {
+      const parsedTasks = JSON.parse(storedTasks);
+      if (Array.isArray(parsedTasks)) {
+        setTasks(parsedTasks);
+      } else {
+        console.error("Stored tasks are not a valid array");
+      }
+    }
+  }, []); // Empty dependency array to ensure this runs once
+
+  // Save tasks to localStorage whenever the tasks state changes
+  useEffect(() => {
+    if (tasks.length > 0) {
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+  }, [tasks]);
 
   const addTask = (taskText: string) => {
     const newTask: Task = {
